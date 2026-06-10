c47d11b32e8399e0ff94d06e4d3ab390c69f0d46
lokendra-wevois <apple@Apples-MacBook-Pro-3.local>
2026-06-10 09:33:43 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ git clone https://github.com/lokendrawevois/pipeline.git
 
 UV_VENV_CLEAR=1
 
-wget https://huggingface.co/bodhicitta/sam3/resolve/main/sam3.pt -O pipeline/sam3.pt
+#wget https://huggingface.co/bodhicitta/sam3/resolve/main/sam3.pt -O pipeline/sam3.pt
+
+#wget https://huggingface.co/Ultralytics/YOLO11/resolve/main/yolo11n.pt -O pipeline/yolo11n.pt
 
 cd pipeline
 
@@ -12,4 +14,25 @@ source .venv/bin/activate
 
 uv pip install -r requirements.txt
 
-.venv/bin/python full_pipeline.py --video sample.mp4 --prompt trash --skip 20
+.venv/bin/python full_pipeline.py --video sample.mp4 --prompt trash --skip 100
+
+LATEST_RUN=$(ls -td outputs/*/ | head -n 1)
+
+echo "Latest run directory: $LATEST_RUN"
+
+.venv/bin/python classify.py \
+    --run_dir "$LATEST_RUN" \
+    --max_workers 10 \
+    --frame_workers 10
+
+cd "${LATEST_RUN}yolo_training" || exit 1
+
+yolo detect train \
+  model=../../../yolo11n.pt \
+  data=data.yaml \
+  epochs=10 \
+  imgsz=640
+
+echo "=== Training output ==="
+ls -lah "$HOME/pipeline/runs/detect/train"
+
